493f46764bda0485eee250f03fa3e6b398343db0
hung8695 <hung8695@gmail.com>
2021-06-01 14:17:46 +0700

    TH tao luong bang Runnable

---
+++ TH-Tao-luong-bang-Runnable/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        RunnableDemo runnableDemo1=new RunnableDemo("Thread-1-HR-Database");
+        runnableDemo1.start();
+        RunnableDemo runnableDemo2=new RunnableDemo("Thread-2-Send-Email");
+        runnableDemo2.start();
+
+        System.out.println("Main thread stopped!!!");
+
+    }
+
+
+}
+
